8fd2e700f4f1916fc150e36db57509370aaa32fc
WoodnycaV <woodnyca.vincent.pro@gmail.com>
2022-10-26 12:55:12 +0200

    Auto stash before merge of "develop" and "origin/develop"

--- L_univers_du_quizz/composants/vue_menue.php
+++ L_univers_du_quizz/composants/vue_menue.php
@@ -13,10 +13,10 @@
 								</label>';
 				if(!isset($_SESSION['login'])){
 					
-					$this->contenu .= " <a href=index.php?module=connexion&action=connexion>Connexion</a> - <a href=index.php?module=connexion&action=inscription>Inscription</a></nav>";
+					$this->contenu .= ' <a class="lienConnexion" id="connexion" href=index.php?module=connexion&action=connexion>Connexion</a><a href=index.php?module=connexion&action=inscription>Inscription</a></nav>';
 					
 				} else{
-					$this->contenu .= " <a href=index.php?module=connexion&action=deconnexion>Déconnexion</a></nav>";
+					$this->contenu .= ' <a class="lienConnexion" id="Deconnexion" href=index.php?module=connexion&action=deconnexion>Déconnexion</a>';
 				}
 			//$this->contenu2 = '<nav><a href="index.php?module=classement">Classement</a> - <a href="index.php?module=cultureG">Culture Générele</a> - <a href="index.php?module=serie">Série</a> - <a href="index.php?module=film">Film</a> - <a href="index.php?module=japanimation">Japanimation</a>';
 		}
--- L_univers_du_quizz/connexion.php
+++ L_univers_du_quizz/connexion.php
@@ -11,7 +11,9 @@
             $user = 'dutinfopw201634';
             $password = 'sahedyja';
 
-            self::$bdd=new PDO('mysql:host=database-etudiants.iut.univ-paris8.fr;dbname=dutinfopw201634',$user,$password);
+           // self::$bdd=new PDO('mysql:host=database-etudiants.iut.univ-paris8.fr;dbname=dutinfopw201634',$user,$password);
+
+                self::$bdd = new PDO('mysql:dbname=bdd;host:localhost;port:80', 'wood', 'Eyh8TaRD@&120226');
         }
 	}
 ?>
