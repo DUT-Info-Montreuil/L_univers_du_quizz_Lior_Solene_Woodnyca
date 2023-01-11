c1f7a66ea8fd4cbad480f440058f2b8faeddabb7
WoodnycaV <101125564+WoodnycaV@users.noreply.github.com>
2023-01-11 16:01:37 +0100

    Update connexion.php

--- L_univers_du_quizz/connexion.php
+++ L_univers_du_quizz/connexion.php
@@ -8,9 +8,9 @@
             $user = 'dutinfopw201634';
             $password = 'sahedyja';
 
-           // self::$bdd=new PDO('mysql:host=database-etudiants.iut.univ-paris8.fr;dbname=dutinfopw201634',$user,$password);
+            self::$bdd=new PDO('mysql:host=database-etudiants.iut.univ-paris8.fr;dbname=dutinfopw201634',$user,$password);
 
-                self::$bdd = new PDO('mysql:dbname=bdd;host:localhost;port:80', 'wood', 'Eyh8TaRD@&120226');
+              //  self::$bdd = new PDO('mysql:dbname=bdd;host:localhost;port:80', 'wood', 'Eyh8TaRD@&120226');
         }
 	}
 ?>
